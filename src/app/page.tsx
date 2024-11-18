9bfa6c9eb81fb8eb69a9b6803758c0f2ddd05d31
ofealves <oifelipealvessjdr@outlook.com>
2024-11-18 01:17:23 -0300

    primeiro commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { Chat } from "@/components/Chat";
+import { ChatProvider } from "@/context/ChatContext";
+import { UserProvider } from "@/context/UserContext";
+
+const Page = () => {
+  return (
+
+    <div className="container mx-auto max-w-lg px-2">
+      <UserProvider>
+        <ChatProvider>
+          <h1 className="text-3xl my-3 text-center">Chat Simples</h1>
+
+          <Chat />
+        </ChatProvider>
+      </UserProvider>
+    </div>
+  )
+};
+
+export default Page;
---
+++ src/components/ChatInput.tsx
@@ -0,0 +1,29 @@
+import { useChat } from "@/context/ChatContext";
+import { KeyboardEvent, useState } from "react";
+
+type Props = {
+    name: string;
+}
+
+export const ChatInput = ( { name }: Props) => {
+    const chatCtx = useChat();
+    const [textInput, setTextInput] = useState('');
+
+    const handleKeyUpAction = (event: KeyboardEvent<HTMLInputElement>) => {
+        if(event.code.toLowerCase() === 'enter') {
+            if(textInput.trim() !== '') {
+                chatCtx?.addMessage(name, textInput.trim());
+                setTextInput('');
+            }
+        }
+    }
+    return (
+        <input 
+        className="w-full bg-transparent text-white text-md outl"
+        placeholder={`${name}, digite uma mensagem (e aperte enter)`}
+        value={textInput}
+        onChange={e => setTextInput(e.target.value)}
+        onKeyUp={handleKeyUpAction}
+        />
+    )
+}
